204021c61d89ee3b336b3985994b0e8cb9f85f9b
Simon <simon.kelly292@mail.dcu.ie>
2024-09-06 00:49:04 +0100

    Fixed join redirects to use joinCode rather than competitionId......

--- src/components/CompetitionRegistration.js
+++ src/components/CompetitionRegistration.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
 import { doc, setDoc, collection, query, where, getDocs } from 'firebase/firestore';
 import emailjs from '@emailjs/browser';
